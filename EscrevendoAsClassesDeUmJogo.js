f03bae5f538e2b1e9ea0ba13059db28e5c4daa8d
Michel Pinheiro <151650711+MichelPinheiro2001@users.noreply.github.com>
2024-02-15 21:32:13 -0300

    desafio Dio teste ok

--- EscrevendoAsClassesDeUmJogo.js
+++ EscrevendoAsClassesDeUmJogo.js
@@ -1,1 +1,27 @@
+let jogador = "Michel";
+let idade = 23;
+let ocupacao = "guerreiro";
+let tipoAtaque = "guerreiro";
+ocupacao = tipo
+//classe para definir os tipos de arma ou ataque de personagens disponiveis
+class TipoClasse {
+    constructor() {
+        this.ataqueMago = "magia";
+        this.ataqueGuerreiro = "espada";
+        this.ataqueMonge = "artes marciais";
+        this.ataqueNinja = "shuriken";
+    }
+}
+//função para definir o tipo de personagem para o algoritmo retornar o tipo de ataque de acordo com a ocapacao do personagem
+let tipoClasse = new TipoClasse();
+if (tipoAtaque === "mago") {
+    tipoAtaque = tipoClasse.ataqueMago;
+} else if (tipoAtaque === "guerreiro") {
+    tipoAtaque = tipoClasse.ataqueGuerreiro;
+} else if (tipoAtaque === "monge") {
+    tipoAtaque = tipoClasse.ataqueMonge;
+} else if (tipoAtaque === "ninja") {
+    tipoAtaque = tipoClasse.ataqueNinja;
+}
 
+console.log("O " + ocupacao + " atacou usando " + tipoAtaque);
